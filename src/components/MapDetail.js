0bcda8baaddca4c3ac5212aa8e1b955d017d0b1a
TinkzBot <zakaria.ziadi92@gmail.com>
2023-09-12 11:36:58 +0200

    Update | Added Icon

--- src/components/MapDetail.js
+++ src/components/MapDetail.js
@@ -3,6 +3,17 @@ import io from 'socket.io-client';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // Import Leaflet's CSS
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 const MapDetail = () => {
   const [trainData, setTrainData] = useState({});
 
